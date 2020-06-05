d667c11c5d5860b8d1c9d0fec0a59071c220d749
Maks027 <maximg.748@gmail.com>
2020-06-06 00:37:29 +0300

    Converting balance string to Money type, with automatic currency parser

--- Browser.rb
+++ Browser.rb
@@ -1,16 +1,18 @@
 require 'rubygems'
 require 'watir'
 require 'webdrivers'
+require 'monetize'
+require 'money'
 
 def balance(br)
   br
-      .div(class: %w[_2tzPNu1unf _22kXFRwS9J])
-      .span(data_semantic: 'header-available-balance-amount')
-      .text_content
+    .div(class: %w[_2tzPNu1unf _22kXFRwS9J])
+    .span(data_semantic: 'header-available-balance-amount')
+    .text_content
 end
 
 def acc_name(br)
-  br.h2(class: "yBcmat9coi").text_content
+  br.h2(class: 'yBcmat9coi').text_content
 end
 
 def new_browser
@@ -22,13 +24,16 @@ end
 
 br = new_browser
 
-fileName = "account.json"
-file = File.open(fileName, "w")
+fileName = 'account.json'
+file = File.open(fileName, 'w')
+
+Monetize.assume_from_symbol = true
+acc_money = balance(br).to_money
 
 
-
-accHash = [:name => acc_name(br) ,
-           :balance => balance(br)]
+accHash = [:name => acc_name(br),
+           :currency => acc_money.currency.to_s,
+           :balance => acc_money.to_f]
 
 
 h = {:account => accHash}
